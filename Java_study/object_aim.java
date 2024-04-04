2cbc1ae71b2abb65722fc967a12f04e60b789539
snowykte0426 <snowykte0426@naver.com>
2024-04-04 20:51:01 +0900

    4월4일 공부한 것

--- Java_study/object_aim.java
+++ Java_study/object_aim.java
@@ -1,16 +1,42 @@
 package Java_study;
+
 import java.util.Scanner;
-public class object_aim {
+
+class old_object_aim {
     double radius = 2.0;
-    public static void object_study1(String [] agrs){
+
+    public static void object_study1(String[] agrs) {
 
         String 출력문 = null;
-        System.out.printf("getVolume 매서드에서 반환 받은 값: %s",getVolume(출력문));
+        System.out.printf("getVolume 매서드에서 반환 받은 값: %s", getVolume(출력문));
     }
 
     public static String getVolume(String methon2) {
-        Scanner in =new Scanner(System.in);
-        methon2 =in.next();
+        Scanner in = new Scanner(System.in);
+        methon2 = in.next();
         return methon2;
+    }
+}
+
+class Phone {
+    String model;
+    int value;
+
+    void print() {
+        System.out.println(value + "만 원짜리 " + model + " 스마트폰");
+    }
+}
+
+public class object_aim {
+    public static void main(String[] args) {
+        Phone myPhone = new Phone();
+        myPhone.model = "갤럭시 S8";
+        myPhone.value = 100;
+        myPhone.print();
+
+        Phone yourPhone = new Phone();
+        yourPhone.model = "G6";
+        yourPhone.value = 85;
+        yourPhone.print();
     }
 }
