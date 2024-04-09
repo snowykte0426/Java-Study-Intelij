75f217cb316291b8d051e50403288ebddfc568d8
snowykte0426 <snowykte0426@naver.com>
2024-04-09 19:46:21 +0900

    4월22일 9교시까지 공부한 것

---
+++ Java_study/Amond_Tistory.java
@@ -0,0 +1,12 @@
+package Java_study;
+
+public class Amond_Tistory {
+    public static void main_1(String[] args) {
+        int[] age = {15, 19, 66, 7};
+        short i = 1;
+        for (int n:age) {
+            System.out.println(i + "번째 사람의 나이는" + n + "살 입니다.");
+            i++;
+        }
+    }
+}
---
+++ Java_study/programmers_java_school_1.java
@@ -0,0 +1,20 @@
+package Java_study;
+
+public class programmers_java_school_1 {   //String Class 강의
+    public static void main(String[] args) {
+        String str1 = "Hello World";
+        String str2 = "Hello World";
+        int[] age = {15, 19, 66, 7};
+        short i = 1;
+        for (int n:age) {
+            System.out.println(i + "번째 사람의 나이는" + n + "살 입니다.");
+            i++;
+        }
+        String str3 = "Hello World";
+        String str4 = "Hello World";
+        if (str1 == str2) System.out.println("str1==str2입니다!!");
+        if (str1 == str3) System.out.println("str1==str3입니다!!");
+        if (str3 == str4) System.out.println("str3==str4입니다!!");
+        else if (str1.equals(str2)) System.out.print("str1==str2이네요????\n");
+    }
+}
