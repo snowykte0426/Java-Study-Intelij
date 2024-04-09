4c7a153b797ed2c99aca3bf5a2cb86862f7d783e
snowykte0426 <snowykte0426@naver.com>
2024-04-09 19:46:21 +0900

    4월22일 기숙사 자습시간에 공부한 것

--- Java_study/Class_and_Method.java
+++ Java_study/Class_and_Method.java
@@ -1,11 +1,14 @@
 package Java_study;
 
 public class Class_and_Method {
+    static int globalScope=10;
     public static void main(String[] args) {
         programmers_java_school_1 Method_T=new programmers_java_school_1();
+        programmers_java_school_1.ex1();
+        System.out.println("");
         Method_T.ex1();
-
+        String tremp1="Oh My Go";
+        System.out.println(globalScope);
+        System.out.println(tremp1.concat("d!"));
     }
 }
-
-class
